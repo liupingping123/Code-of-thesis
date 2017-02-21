101bcde2dd426a8861469c1e2eae52c76c964758
TongzheZhang <richard10ztz@gmail.com>
2017-02-21 20:00:04 +0800

    张桐喆

--- 4_3_modeling_tensor_co/main_modeling_tensor_co.m
+++ 4_3_modeling_tensor_co/main_modeling_tensor_co.m
@@ -13,7 +13,7 @@ dim3=2;
 %V保留的主成分维度
 dim_v1=4;
 dim_v2=70;
-dim_v3=1;
+dim_v3=2;
 %% 构建张量流
 [re_tensor_flow, tensor_flow] = con_tensor_flow(total_len,dim1,dim2,dim3);
 
@@ -21,30 +21,46 @@ dim_v3=1;
 [V1,V2,V3] = re_co_tensor_tucker(tensor_flow,y_incre,train_num,dim1,dim2,dim3,dim_v1,dim_v2,dim_v3);
 
 %% 利用V1,V2,V3得到利用相关性重建的张量流
+re_tensor_flow_mat = zeros(6,100,3,total_len);
 re_co_tensor_flow = cell(1,total_len);
+re_co_tensor_flow_mat = zeros(dim_v1,dim_v2,dim_v3,total_len);
 for i = 1:total_len
-    re_co_tensor_flow{i} = re_co_tensor_tucker_single(tensor_flow{i},V1,V2,V3,dim1,dim2,dim3);
-    one_tensor=re_co_tensor_flow{i};
-    featurelist(i,:)= double(tenmat(one_tensor,3));
+    [re_co_tensor_flow{i},re_co_tensor_flow_mat(:,:,:,i)] = re_co_tensor_tucker_single(tensor_flow{i},V1,V2,V3,dim1,dim2,dim3);
+    re_tensor_flow_mat(:,:,:,i) =  re_tensor_flow{i};
+    %one_tensor=re_co_tensor_flow{i};
+    %featurelist(i,:)= double(tenmat(one_tensor,3));
 end
-featurelist=normr(featurelist);
-save featurelist featurelist
+%featurelist=normr(featurelist);
 % %% 回归训练
-% cc=0.1;
-[A,B,C,bias] = tensor_reg(re_co_tensor_flow,y_incre,total_len,train_num,test_num,2,dim_v1,dim_v2,dim_v3);
-% 
+%[A,B,C,bias] = tensor_reg(re_co_tensor_flow,y_incre,total_len,train_num,test_num,2,dim_v1,dim_v2,dim_v3);
 % %% 检验训练的结果
 % right_num = test_ABCbias(A,B,C,bias,re_co_tensor_flow,y_incre,total_len,train_num,test_num)
-% tempmodel = svmtrain(y_incre(1:177)', featurelist(1:177,:),  '-c 100 -s 4 -t 0');
-% pred_price = svmpredict(y_incre(178:221)', featurelist(178:221,:), tempmodel);
-% model = regress(y_incre(1:177)',featurelist(1:177,:));
-% predicted_label=predict(y_incre(178:221)', featurelist(178:221,:), model)
-t1s=0;
-for i=1:44
-    if pred_price(i)*y_incre(i+177)>0  
-        t1s=t1s+1;
-    end
+%% general tensor ridge regression
+lambda = 0.001;
+R = 3;
+
+[U, d, err] = genTensorRegression(tensor(re_tensor_flow_mat(:,:,:,1:train_num)),y_incre(1:train_num)',lambda,R);
+model.U = U;
+model.b = d;
+model.train_err = err;
+
+%% 测试
+ten_U = ktensor(U);
+%ten_U = tensor(ten_U);
+
+pred_price = [];
+for i = 1:test_num
+    tempFeature = tensor(re_tensor_flow_mat(:,:,:,train_num+i));
+    YPt = innerprod(tempFeature, ten_U)+d;
+    pred_price = [pred_price;YPt]
 end
+t1s=0;
+%% 结果
+ for i=1:44
+     if pred_price(i)*y_incre(i+177)>0
+         t1s=t1s+1;
+     end
+ end
 save pred_price pred_price
 t1s
 t1s/44
--- 4_3_modeling_tensor_co/re_co_tensor_tucker_single.m
+++ 4_3_modeling_tensor_co/re_co_tensor_tucker_single.m
@@ -1,4 +1,4 @@
-function [ B ] = re_co_tensor_tucker_single( A,V1,V2,V3,dim1,dim2,dim3 )
+function [ B Bmat] = re_co_tensor_tucker_single( A,V1,V2,V3,dim1,dim2,dim3 )
 %re_co_tensor_tucker 此处显示有关此函数的摘要
 %   此处显示详细说明
 %% 按不同的模展开张量
@@ -22,6 +22,6 @@ U2 = V2'*U2;
 U3 = V3'*U3;
 %% 重构
 B = ttm(S,{U1,U2,U3}); %tucker的三方面矩阵就是U1，U2，U3
-
+Bmat = double(B);
 end
 
